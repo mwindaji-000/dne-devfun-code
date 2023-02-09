67a88a1ae13873ef1976727a415b220f3ae93ae7
Threat Hunter <kigunda@threathunter.ca>
2023-02-08 21:05:08 -0500

    Test Submission

--- intro-python/interpreter-basics/hello.py
+++ intro-python/interpreter-basics/hello.py
@@ -21,4 +21,71 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-print("Hello Python!!  ...at least I didn't say World.")
+# print("Hello Python!!  ...at least I didn't say World.")
+# print("Today I will learn something new.\nTomorrow I will learn some more.")
+
+# "duplex {}".format("full")
+# "interface {name}, port {port}".format(name="gigabitethernet", port="0/1")
+headers1 = "device hostname ipaddress".split(" ")
+headers2 = ",".join(headers1)
+print(headers1,"\n",headers2)
+
+# formatted literal strings
+tool = "git clone "
+host = "https://github.com/"
+org = "CiscoDevNet"
+repo = "/netprog_basics"
+command = f"{tool}{host}{org}{repo}"
+print(command)
+
+# inputs
+# num1 = int(input("What is the first number "))
+# num2 = int(input("What is the second number "))
+num1,num2 = 2, 4
+
+# functions
+def add(num1, num2):
+     result = num1 + num2
+     return result
+
+print(add(num1, num2))
+
+# data types()
+# lists
+lookup = ["ssh", "tcp", "ftp", 19.2, 20.1]
+lookup[2] = "udp"
+print(lookup[2])
+print(lookup.append("ftp"))
+print(lookup)
+
+# tuples
+territory = ("FillmoreDC", "NorthWestRegion")
+print(territory[0])
+
+# dictionaries
+devicehost = {"ipv4address": "192.168.0.10", "traffic": "inbound", "port": 40044}
+print(devicehost["traffic"])
+devicehost["ipv4address"] = "192.168.0.111"
+devicehost["alert"] = "Never"
+print(devicehost)
+# print(dir(devicehost)) # shows methods => in a list
+
+# COLLECTIONS MODULES
+# example 1
+from collections import OrderedDict
+metric_l = dict()
+metric_l["finance"] = 95
+metric_l["health"]= 25
+metric_l["comms"] =  55
+metrics = OrderedDict()
+metrics["finance"] = 95
+metrics["health"] = 25
+metrics["comms"] = 55
+print(metrics)
+print("metrics_l", metric_l)
+metrics["alocal"] = 5
+print(metrics)
+
+# loops
+for industry, head in metrics.items():
+    print("industry:= {ind}, head count:= {pax}".format(ind=industry, pax=head))
